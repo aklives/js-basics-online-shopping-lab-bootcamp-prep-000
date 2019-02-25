cae2be3978e7c4ebb54fed6426be5578d26db783
Adam Kearney <aklives@hotmail.com>
2019-02-25 17:43:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function addToCart(item) {
   var itemName = item;
   var itemPrice = parseInt(Math.random() * 100);
   cart.push({itemName : itemPrice});
+  console.log(itemName);
   return `${itemName} has been added to your cart.`;  
 }
 
