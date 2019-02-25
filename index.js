3ec500c7f21edacc1c1c032c154bfe3e89d13929
Adam Kearney <aklives@hotmail.com>
2019-02-25 19:45:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ function addToCart(item) {
 }
 
 function viewCart() {
+  if (cart.length === 0){
+    return 
+  }
   var sentence = 'In your cart, you have';
   for (var i = 0; i < cart.length-1; i++){
     sentence += ' ';
